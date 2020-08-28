ab423d93d58f56ee83660ce010a4761163209a73
Solar-Taxi-Software-Team <jemima@solartaxi.co>
2020-08-28 16:05:20 +0000

    onIncrement and onDecrement logic

--- src/App.js
+++ src/App.js
@@ -10,14 +10,27 @@ const data = [
 
 // Counter Component
 class Counter extends Component {
+
+  onIncrement = e => {
+    this.props.onIncrement(1);
+  };
+
+  onDecrement = () => {
+    this.props.onIncrement(-1);
+  };
+
   render() {
     const { value } = this.props;
     return (
       <div className="counter">
         <b>{value}</b>
         <div className="counter-controls">
-          <button className="button is-danger is-small">-</button>
-          <button className="button is-success is-small">+</button>
+          <button className="button is-danger is-small"
+          onClick={this.onDecrement}
+          >-</button>
+          <button className="button is-success is-small"
+          onClick={this.onIncrement}
+          >+</button>
         </div>
       </div>
     );
@@ -27,13 +40,28 @@ class Counter extends Component {
 class App extends Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      data: [...data]
+    };
   }
 
+  onIncrement = (counter, increment) => {
+    const newValue = counter.value + increment;
+    this.setState({
+      data: data.map(item => {
+        item.value = item.id === counter.id ? newValue : item.value;
+        return item;
+      })
+    });
+  };
+
   render() {
+    const { data } = this.state;
     return (
       <div>
         {data.map((counter) => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter key={counter.id} value={counter.value} 
+          onIncrement={this.onIncrement.bind(this, counter)}/>
         ))}
       </div>
     );
